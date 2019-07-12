dc08eb57e8d597ee5643cf894b7c3fd3555c1119
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-07-12 16:39:13 -0400

    auth route

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -2,9 +2,19 @@ const express = require("express");
 const router = express.Router()
 const auth = require("../../middleware/auth")
 
+const User = require('../../models/User');
+
 //@route Get api/users
 //@desc Test Route
 //@access Public
-router.get("/", auth, (req,res) => res.send("Auth Route"));
+router.get("/", auth, (req,res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router
