370520dbedff3464d0c69ba1866e5e451af831be
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-10-22 00:50:33 -0400

    begining frontend

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -207,25 +207,27 @@ router.post(
   }
 );
 
-//@route DELETE api/posts/comment/:id/:comment_id
-//@desc Delete comment on a post
-//@access Private
-router.delete('/comment/:id/comment_id', auth, async, (req, res) => {
+// @route    DELETE api/posts/comment/:id/:comment_id
+// @desc     Delete comment
+// @access   Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
-    //pull out comment
-    const comment = post.comment.find(comment => comment.id === req.params.comment_id);
+    // Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
 
-    //check if comment exists
-    if(!comment){
+    // Make sure comment exists
+    if (!comment) {
       return res.status(404).json({ msg: 'Comment does not exist' });
-   }
+    }
 
-   //check user
-   if(comment.user.toString() !== req.user.id){
-    return res.status(401).json({ msg: 'User not authorized' });
-   }
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
 
     // Get remove index
     const removeIndex = post.comments
