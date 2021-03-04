d41671a8caaa8dcbe1e0ea51a635141b9f732d46
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-03-03 21:05:45 -0500

    more profile dashboard setup

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,6 +9,7 @@ import {
   LOGIN_FAIL,
   LOGIN_SUCCESS,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 
 //Load User
@@ -94,5 +95,6 @@ export const login = (email, password) => async (dispatch) => {
 
 //logout /clear profile
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
